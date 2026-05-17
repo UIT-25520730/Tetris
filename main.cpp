754c654d6fdfc42c42a55e27a474ee5fad62de18
25520460-vohoanggia <25520460@gm.uit.edu.vn>
2026-05-17 17:18:24 +0700

    Thêm bảng điểm cá nhân cao nhất

--- main.cpp
+++ main.cpp
@@ -155,29 +155,7 @@ void loadHighScores() {
     }
 }
 
-void saveHighScore(int currentScore) {
-    highScores.push_back(currentScore); 
-    
-    for (size_t i = 0; i < highScores.size(); i++) {
-        for (size_t j = i + 1; j < highScores.size(); j++) {
-            if (highScores[i] < highScores[j]) {
-                int temp = highScores[i];
-                highScores[i] = highScores[j];
-                highScores[j] = temp;
-            }
-        }
-    }
-    
-    if (highScores.size() > 5) {
-        highScores.resize(5);
-    }
-    
-    ofstream file("highscore.txt");
-    for (int s : highScores) {
-        file << s << "\n";
-    }
-    file.close();
-}
+
 
 
 void enableANSIColors(HANDLE hOut)
@@ -382,15 +360,6 @@ void draw()
             }
             s += nextUI;
         }
-        if (i == 13) s += "    \x1b[91m[ TOP 5 HIGH SCORES ]\x1b[0m";
-        
-        
-        if (i >= 14 && i <= 18) {
-            int rank = i - 13; 
-            if ((size_t)(rank - 1) < highScores.size()) {
-                s += "     " + to_string(rank) + ". " + to_string(highScores[rank - 1]);
-            }
-        }
         s += "\n";
     }
     cout << s;
@@ -442,7 +411,7 @@ int main()
     
     SetConsoleOutputCP(CP_UTF8);
     srand((unsigned)time(0));
-    loadHighScores();
+  
     initBoard();
     initGame();
     HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -555,7 +524,7 @@ int main()
                 delete currentPiece;
                 spawnNextPiece();
                 if (!canMove(0, 0, x, y)) { 
-                    saveHighScore(score);
+                    
                     draw();
                     break;
                 }
@@ -565,7 +534,7 @@ int main()
             }
 
             if (GetAsyncKeyState('Q') & 0x8000) {
-            saveHighScore(score); // <- Thêm dòng này ở đây
+            
             break;
             }
 
@@ -589,7 +558,7 @@ int main()
               spawnNextPiece();
                 if (!canMove(0, 0, x, y))
                 {
-                    saveHighScore(score);
+                    
                     system("cls");
                     cout << "GAME OVER!";
                     break;
